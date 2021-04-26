52fadc966c1dac9633e693f82094b972eaf3f5cb
ankanuj <s.singhanuj132@gmail.com>
2021-04-26 17:33:49 +0530

    making markdown, created text area and output area

--- src/components/markdown-components/markdown-component.jsx
+++ src/components/markdown-components/markdown-component.jsx
@@ -1,9 +1,37 @@
 import React , {Component} from 'react';
 
+import './markdown.css';
+
 class MarkDown extends Component {
+    
+    constructor(){
+        super();
+        
+        this.state = {
+            markDownText:'hi,**anuj**',
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e){
+        this.setState({
+            markDownText:e.target.value
+        });
+    }
     render(){
+    
         return(
-            <h1>MarkDown Application</h1>
+            <div className = "markdown-block">
+                <h1 className = "markdown-heading">MarkDown Application</h1>
+                
+                    <textarea
+                    onChange = {this.handleChange}
+                    value = {this.state.markDownText}
+                    />
+                    <h1 className = "output-header">Output area </h1>
+                    <div className = "output-block"> {this.state.markDownText}</div>
+            </div>
+
         );
     }
 } 
