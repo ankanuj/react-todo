2e7a3e01c68ef39143acd74b2973a3d541973363
ankanuj <s.singhanuj132@gmail.com>
2021-04-29 00:36:54 +0530

    remove bugs and updated markdown features

--- src/components/markdown-components/markdown-component.jsx
+++ src/components/markdown-components/markdown-component.jsx
@@ -2,15 +2,6 @@ import React , {Component} from 'react';
 import {Container} from 'react-bootstrap';
 
 import './markdown.css';
-
-// const priority={
-//   '**': 1,
-//   '*': 2,
-//   '#': 3,
-//   '@': 4,
-//   '~': 5,
-// };//object
-
 class MarkDownComponent extends Component {   
   constructor() {
     super();
@@ -38,39 +29,57 @@ class MarkDownComponent extends Component {
     let textColor="white";
     let strikeText= "";
     let italicText= "";
+    var regExp = /[a-zA-Z]/g; 
     inputCopy.split("\n").forEach((str) => {
       str.split(" ").forEach(elem =>{
         const strLen = elem.length;
-        var firstIndexOfAsterisk = elem.indexOf("*");
-        var lastIndexAst = elem.indexOf("*",firstIndexOfAsterisk+2);
+        var firstIndexOfAsterisk = elem.indexOf("*",0);
+        var lastIndexAst = elem.lastIndexOf("*",strLen);
+        console.log(elem.substring(firstIndexOfAsterisk,firstIndexOfAsterisk+2));
+        //checking for ** tag 
         if(
-        strLen > 4 &&
         '**' === elem.substring(firstIndexOfAsterisk,firstIndexOfAsterisk+2) &&
-        "**" === elem.substring(lastIndexAst, lastIndexAst+2) 
-      ){
-        boldStr = elem.substring(firstIndexOfAsterisk+2, lastIndexAst);
+        "**" === elem.substring(lastIndexAst-1, lastIndexAst+1) && strLen>2 ) 
+        {
+          
+        boldStr = elem.substring(firstIndexOfAsterisk+2, lastIndexAst-1);
         if(firstIndexOfAsterisk > 0){
           var beforeAsteriskText = elem.substring(0,firstIndexOfAsterisk);
-          var afterAsteriskText = elem.substring(lastIndexAst+2, strLen+1);
+          var afterAsteriskText = elem.substring(lastIndexAst+1, strLen+1);
+          console.log('hi');
           inputCopy = inputCopy.replace(elem,beforeAsteriskText+"<b>"+boldStr +"</b>"+afterAsteriskText);
 
-        }
-        else{
+          }
+        else if(regExp.test(boldStr)){
           inputCopy = inputCopy.replace(elem,"<b>"+boldStr +"</b>");
+          }
         }
-      }
-      else if('*' === elem.substring(0,1) && '*' === elem.substring(strLen-1,strLen) && strLen > 3 ){
-        italicText= elem.substring(1,strLen-1);
-        inputCopy= inputCopy.replace(elem,('<i>'+italicText+'</i>'))
-      }
+        //checking for single *
+        else if(
+        '*' === elem.substring(firstIndexOfAsterisk,firstIndexOfAsterisk+1) &&
+        "*" === elem.substring(lastIndexAst, lastIndexAst+1) && strLen>2 && regExp.test(elem) )
+        {
+        italicText= elem.substring(firstIndexOfAsterisk+1, lastIndexAst);
+        if(firstIndexOfAsterisk > 0){
+          beforeAsteriskText = elem.substring(0,firstIndexOfAsterisk);
+          afterAsteriskText = elem.substring(lastIndexAst+1, strLen+1);
+          inputCopy = inputCopy.replace(elem,beforeAsteriskText+'<i>'+italicText+'</i>'+afterAsteriskText);
+          }
+        else{
+          inputCopy= inputCopy.replace(elem,('<i>'+italicText+'</i>'))
+          }
+        }
+      //checking for #   
       else if('#' === elem.substring(0,1) && strLen>1){
         hashtag = elem.substring(1,strLen);
         inputCopy = inputCopy.replace(elem,('<span style="color:'+color+';">'+hashtag+'</span>'))
       }
+      //checking for @
       else if('@' === elem.substring(0,1) && strLen>1){
         mentionText= elem.substring(1,strLen);
         inputCopy= inputCopy.replace(elem,('<span style="background-color:'+bgColor+'; color:'+textColor+';">@'+mentionText+'</span>'))
       }
+      //checking for ~
       else if('~' === elem.substring(0,1) && strLen>1){
         strikeText= elem.substring(1,strLen);
         inputCopy= inputCopy.replace(elem,('<strike>'+strikeText+'</strike>'))
